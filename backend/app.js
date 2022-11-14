062b72f36cf963508e9390e3baa8dc4608cabab5
Yan Rivera <yan.rivera.6@gmail.com>
2022-11-14 10:58:56 -0800

    Setup CSRF and CORS protection

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const csurf = require("csurf");
+
+const cors = require("cors");
+const { isProduction } = require("./config/keys");
 
 const usersRouter = require("./routes/api/users");
+const csrfRouter = require("./routes/api/csrf");
 
 const app = express();
 
@@ -11,6 +16,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+  app.use(cors());
+}
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
 app.use("/api/users", usersRouter);
+app.use("/api/csrf", csrfRouter);
 
 module.exports = app;
