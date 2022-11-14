16cc0668ecc925b3b913867b7f2d6f085ff5dad0
Yan Rivera <yan.rivera.6@gmail.com>
2022-11-14 11:28:12 -0800

    Setup passport jwt and auth

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const passport = require("passport");
+const { loginUser, restoreUser } = require("../../config/passport");
+const { isProduction } = require("../../config/keys");
 
 router.get("/", function (req, res, next) {
   res.json({
@@ -42,7 +44,7 @@ router.post("/register", async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       } catch (err) {
         next(err);
       }
@@ -59,8 +61,21 @@ router.post("/login", async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
+});
+
+router.get("/current", restoreUser, (req, res) => {
+  if (!isProduction) {
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email,
+  });
 });
 
 module.exports = router;
