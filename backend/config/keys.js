7b44a2bb2530c42c1305982ed3b56285f02fd04f
Yan Rivera <yan.rivera.6@gmail.com>
2022-11-16 17:09:26 -0800

    Testing create events

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -2,4 +2,6 @@ module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY,
   mongoURI: process.env.MONGO_URI,
   isProduction: process.env.NODE_ENV === "production",
+  s3AccessKey: process.env.S3_ACCESS_KEY,
+  s3AccessSecret: process.env.S3_ACCESS_SECRET,
 };
