de0e0d705e9baa5e638625e4450fc732b848f417
nelsenw <wnelsen9@gmail.com>
2022-11-30 10:01:08 -0800

    final review card

--- frontend/src/components/UserPage/UserModal.jsx
+++ frontend/src/components/UserPage/UserModal.jsx
@@ -39,25 +39,23 @@ export default function UserModal({ userId }) {
       <img src={peopleImgs[Math.floor(Math.random()*3)]} className='person-img'></img>
       <div className="user-modal-info">
         <h1 id="user-modal-info-username">{user?.username}</h1>
-      <h2>Host Reviews: {reviewsAverage('hostReviews')}</h2>
-      <ul>
+      <h2>Host Reviews: <span>{reviewsAverage('hostReviews')} <i className="fa-regular fa-star"></i></span></h2>
+      <ul className="reviews-list">
         {user?.hostReviews?.map((review) => {
           return (
-            <li>
-              <h3>{review.title}</h3>
-              <span>{review.rating}</span>
+            <li className="review-bubble">
+              <h3>{review.title} <span>{review.rating} <i className="fa-regular fa-star"></i></span></h3>
               <p>{review.body}</p>
             </li>
           );
         })}
       </ul>
-      <h2>Guest Reviews:</h2>
-      <ul>
+      <h2>Guest Reviews:  <span>{reviewsAverage('guestReviews')} <i className="fa-regular fa-star"></i></span></h2>
+      <ul className="reviews-list">
         {user?.guestReviews?.map((review) => {
           return (
-            <li>
-              <h3>{review.title}</h3>
-              <span>{review.rating}</span>
+            <li className="review-bubble">
+              <h3>{review.title} <span>{review.rating} <i className="fa-regular fa-star"></i></span></h3>
               <p>{review.body}</p>
             </li>
           );
